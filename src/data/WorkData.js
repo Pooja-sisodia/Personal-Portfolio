55f75f64ee25bc05e5dcc4a415a18a1e57835293
Pooja-sisodia <poojasisodia406@gmail.com>
2023-04-10 03:20:59 +0530

    ALL DONE

---
+++ src/data/WorkData.js
@@ -0,0 +1,30 @@
+export const WorkData = [
+	{
+		title: "Product Management",
+		desc: "an eCommerce website's backend part with multiple APIs.",
+		tech: ["NodeJs", "MongoDb", "ExpressJs", "AWS", "Axios"],
+		gitlink: "https://github.com/Pooja-sisodia/Shopping-Cart/tree/shopping-cart",
+		site: "https://github.com/Pooja-sisodia/Shopping-Cart/tree/shopping-cart",
+	},
+	{
+		title: "Blogging Site",
+		desc: "A blog website where you can post your ideas and view those of others",
+		tech: ["NodeJs", "MongoDb", "ExpressJs"],
+		gitlink: "https://github.com/Pooja-sisodia/mini-blogsite-group-5/tree/project-1/mini-blog-site",
+		site: "https://github.com/Pooja-sisodia/mini-blogsite-group-5/tree/project-1/mini-blog-site",
+	},
+	{
+		title: "Book Management",
+		desc: "books library website where users can give review on books.",
+		tech: ["NodeJs", "MongoDb", "ExpressJs"],
+		gitlink: "https://github.com/Pooja-sisodia/Project-Books-Management/tree/project/booksManagement",
+		site: "https://github.com/Pooja-sisodia/Project-Books-Management/tree/project/booksManagement",
+	},
+	{
+		title: "URL Shortner",
+		desc: "A website to short long URLs",
+		tech: ["NodeJs", "MongoDb", "ExpressJs", "Redis", "Axios"],
+		gitlink: "https://github.com/Pooja-sisodia/ShortUrl-Project/tree/project/urlShortner",
+		site: "https://github.com/Pooja-sisodia/ShortUrl-Project/tree/project/urlShortner",
+	}
+];
