2b0531066b12ae7a71795276fc48064340ebb01d
Pooja-sisodia <poojasisodia406@gmail.com>
2023-05-30 02:02:15 +0530

    TEMP CONTROL AND MENU CARD UPDATE

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -1,11 +1,26 @@
 export const WorkData = [
 	{
 		title: "Todo Web",
-		desc: "an eCommerce website's backend part with multiple APIs.",
-		tech: ["React JS"],
+		desc: "Your all-in-one task management solution for organizing, tracking, and completing your to-do list.",
+		tech: ["React JS", "HTML", "CSS", "JAVASCRIPT"],
 		gitlink: "https://github.com/Pooja-sisodia/Todo-Website.git",
 		site: "https://todo-weblist.netlify.app/",
 	},
+	{
+		title: "Temperature Control Web",
+		desc: "Stay comfortable in any climate with our intelligent temperature control website.",
+		tech: ["React JS", "HTML", "CSS", "JAVASCRIPT"],
+		gitlink: "https://github.com/Pooja-sisodia/Temperature-control.git",
+		site: "https://tempcontrolweb.netlify.app/",
+	},
+	{
+		title: "Menu Card",
+		desc: "Explore a world of flavors at your fingertips with our enticing cafe menu card website.",
+		tech: ["React JS", "HTML", "CSS", "JAVASCRIPT"],
+		gitlink: "https://github.com/Pooja-sisodia/menu-card.git",
+		site: "https://menukart.netlify.app/",
+	},
+
 	{
 		title: "Product Management",
 		desc: "an eCommerce website's backend part with multiple APIs.",
